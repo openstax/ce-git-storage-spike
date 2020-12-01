a250f328d71ae6ef766bcf8a1d431b99c84d109c
tomjw64 <tom.jw64@gmail.com>
2020-12-01 11:15:44 -0600

    Include chosen metadata from json in content

--- update_metadata.py
+++ update_metadata.py
@@ -1,21 +1,29 @@
 """Update metadata in repository during content sync"""
 
 import sys
+import json
 from pathlib import Path
 from lxml import etree
 
+
+NS_MDML = "http://cnx.rice.edu/mdml"
+NS_CNXML = "http://cnx.rice.edu/cnxml"
 METADATA_ACCEPT_TAGS = [
-    "{http://cnx.rice.edu/mdml}title",
-    "{http://cnx.rice.edu/mdml}abstract",
-    "{http://cnx.rice.edu/mdml}content-id"
+    f"{{{NS_MDML}}}title",
+    f"{{{NS_MDML}}}abstract",
+    f"{{{NS_MDML}}}content-id"
 ]
+METADATA_ADDED_TAGS_FROM_JSON = {
+    "id": "uuid",
+    "canonical": "canonical-book-uuid"
+}
 
 
 def filter_accepted_tags(cnxml_doc, accept_tags):
     """Remove metadata fields based upon an accepted tags list"""
     metadata = cnxml_doc.xpath(
         "//x:metadata",
-        namespaces={"x": "http://cnx.rice.edu/cnxml"}
+        namespaces={"x": NS_CNXML}
     )[0]
 
     for elem in metadata:
@@ -23,17 +31,38 @@ def filter_accepted_tags(cnxml_doc, accept_tags):
             metadata.remove(elem)
 
 
+def add_metadata_from_json(cnxml_doc, metadata_json, added_tags):
+    metadata = cnxml_doc.xpath(
+        "//x:metadata",
+        namespaces={"x": NS_CNXML}
+    )[0]
+
+    for from_key, to_tag in added_tags.items():
+        value = metadata_json[from_key]
+        element = etree.Element(f"{{{NS_MDML}}}{to_tag}")
+        element.text = value
+        element.tail = "\n"
+        metadata.append(element)
+
+
 def main():
     modules_dir = Path(sys.argv[1]).resolve(strict=True)
 
-    cnxml_files = [
-        cf for cf in modules_dir.glob("**/*")
+    module_files = [
+        (
+            cf.resolve(strict=True),
+            (cf / ".." / "metadata.json").resolve(strict=True)
+        )
+        for cf in modules_dir.glob("**/*")
         if cf.is_file() and cf.name == "index.cnxml"
     ]
 
-    for cnxml_file in cnxml_files:
+    for cnxml_file, metadata_file in module_files:
         cnxml_doc = etree.parse(str(cnxml_file))
+        metadata_json = json.load(metadata_file.open())
+
         filter_accepted_tags(cnxml_doc, METADATA_ACCEPT_TAGS)
+        add_metadata_from_json(cnxml_doc, metadata_json, METADATA_ADDED_TAGS_FROM_JSON)
 
         with open(cnxml_file, "wb") as f:
             cnxml_doc.write(f, encoding="utf-8", xml_declaration=False)
